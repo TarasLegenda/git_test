11bc5082621f8ead6b8803e0d6f8ec37be7f651c
cigan <subjects.gm.1@gmail.com>
2023-07-14 00:46:59 +0300

    end

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
 	if request.method == 'POST':
-		n_prox = request.form['n_prox']
-		n_loop = request.form['n_loop']
-		url = request.form['url']
+		# n_prox = request.form['n_prox']
+		# n_loop = request.form['n_loop']
+		# url = request.form['url']
 		
-		dos(url=url, number=int(n_loop), proxy_list=get_proxy(int(n_prox)))
+		dos(url="http://127.0.0.1:5001/", number=3, proxy_list=get_proxy(int(3)))
 		
 		return 1
 	return render_template('index.html')
 
 if __name__ == "__main__":
-	app.run(debug=True)
+	app.run(debug=True, host='127.0.0.1', port=5000)
